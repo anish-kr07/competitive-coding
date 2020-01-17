7d75a0b441a498ef8082564e1fc23c6c21135bd4
Anish <green_arrow@Anishs-MacBook-Air.local>
2020-01-17 08:22:31 +0530

    771. Jewels and Stones via hashMap/Set

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,9 +6,11 @@ public class HelloWorld {
         System.out.println("HI... I am Anish and this is my  Hellow World program");
 
         //Two Sum
-        TwoSum twoSum = new TwoSum();
-        int a[] = twoSum.twoSum(new int[]{1, 2, 5, 6, 7}, 9);
-        System.out.println(a[0] + "  " + a[1]);
+//        TwoSum twoSum = new TwoSum();
+//        int a[] = twoSum.twoSum(new int[]{1, 2, 5, 6, 7}, 9);
+//        System.out.println(a[0] + "  " + a[1]);
+        JewelsAndStones jewelsAndStones = new JewelsAndStones();
+        System.out.println("No Of jewsels:" + jewelsAndStones.numJewelsInStones("aA","aAAbbbb"));
 
     }
 
---
+++ src/JewelsAndStones.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class JewelsAndStones {
+    public int numJewelsInStones(String J, String S) {
+//        1 method
+//        Map<Character,Integer> hashMap = new HashMap<>();
+//        int totalJewels = 0;
+//        for(Character ch : J.toCharArray()){
+//            char ch = J.charAt(i);
+//            hashMap.put(ch,0);
+//        }
+//        for(Character ch :S.toCharArray()){
+//            char ch = S.charAt(i);
+//            if(hashMap.get(ch) != null){
+//                not required
+//                hashMap.put(ch,hashMap.get(ch)+1);
+//                totalJewels++;
+//            }
+//        }
+//        return totalJewels;
+//        2 method
+//        Set<Character> set = new HashSet<>();
+//        int totalJewels = 0;
+//        for(Character ch : J.toCharArray()){
+//            set.add(ch);
+//        }
+//        for(Character ch :S.toCharArray()){
+//            if(set.contains(ch))
+//                totalJewels++;
+//        }
+//        return totalJewels;
+//        3 method
+        int totalJewels = 0;
+        for(Character ch :S.toCharArray()){
+            if(J.contains(ch.toString()))
+                totalJewels++;
+        }
+        return totalJewels;
+    }
+}
