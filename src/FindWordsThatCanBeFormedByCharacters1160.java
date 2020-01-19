35c7f8518d9ce7b7b01763b5722276efb918d757
AnIsh Sharma <green_arrow@Anishs-MacBook-Air.local>
2020-01-19 21:27:47 +0530

    811. Subdomain Visit Coun and Find Words That Can Be Formed by Characters

---
+++ src/FindWordsThatCanBeFormedByCharacters1160.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindWordsThatCanBeFormedByCharacters1160 {
+    public int countCharacters(String[] words, String chars) {
+        Map<Character,Integer> map = new HashMap<>();
+        for(Character ch : chars.toCharArray()){
+            if(!map.containsKey(ch)){
+                map.put(ch,1);
+            }
+            else{
+                map.replace(ch,map.get(ch)+1);
+            }
+        }
+        int sum = 0;
+        for(String st: words){
+            boolean flag = false;
+            HashMap<Character,Integer> hashMap = new HashMap<>();
+            hashMap.putAll(map);
+            for(Character ch : st.toCharArray()){
+                if(!hashMap.containsKey(ch) || hashMap.get(ch) == 0){
+                    flag = false;
+                    break;
+                } else {
+                    flag = true;
+                    hashMap.replace(ch,hashMap.get(ch)-1);
+                }
+            }
+
+            if(flag) sum+=st.length();
+        }
+//        System.out.println("output "+sum );
+        return sum;
+    }
+}
---
+++ src/SubdomainVisitCount811.java
@@ -0,0 +1,32 @@
+import com.sun.jdi.event.StepEvent;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class SubdomainVisitCount811 {
+    public List<String> subdomainVisits(String[] cpdomains) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        List<String> list = new ArrayList<>();
+        for (String st : cpdomains) {
+            String domains[] = st.split(" ");
+            int count = Integer.parseInt(domains[0]);
+            int numberOfSubdomains = domains[1].split("\\.").length;
+            StringBuilder stringBuilder = new StringBuilder(domains[1]);
+
+            hashMap.put(domains[1],
+                    (hashMap.get(domains[1]) == null ? 0 : hashMap.get(domains[1])) + count);
+
+            for (int i = 1; i < numberOfSubdomains; i++) {
+                String sub = stringBuilder.substring(stringBuilder.indexOf(".") + 1);
+                hashMap.put(sub, (hashMap.get(sub) == null ? 0 : hashMap.get(sub)) + count);
+                String st1 = stringBuilder.substring(stringBuilder.indexOf(".") + 1);
+                stringBuilder = new StringBuilder(st1);
+            }
+        }
+        for (String st : hashMap.keySet()) {
+            list.add(hashMap.get(st) + " " + st);
+        }
+        return list;
+    }
+}
