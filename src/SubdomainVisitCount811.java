8c036797c5d236416486b59457587bd3c8205666
AnIsh Sharma <green_arrow@Anishs-MacBook-Air.local>
2020-01-23 09:37:48 +0530

    811. Subdomain Visit Coun [using getOrDefault(lower runt time colexity)]

--- src/SubdomainVisitCount811.java
+++ src/SubdomainVisitCount811.java
@@ -14,8 +14,7 @@ public class SubdomainVisitCount811 {
             int numberOfSubdomains = domains[1].split("\\.").length;
             StringBuilder stringBuilder = new StringBuilder(domains[1]);
 
-            hashMap.put(domains[1],
-                    (hashMap.get(domains[1]) == null ? 0 : hashMap.get(domains[1])) + count);
+            hashMap.put(domains[1], hashMap.getOrDefault(domains[1],0)+ count);
 
             for (int i = 1; i < numberOfSubdomains; i++) {
                 String sub = stringBuilder.substring(stringBuilder.indexOf(".") + 1);
